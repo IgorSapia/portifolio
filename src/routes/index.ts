3baab6651c224bfe672dc3f1dc9b0e81d6555746
IgorSapia <igor.sapia@gmail.com>
2022-07-31 17:32:31 -0300

    Arrumando Topo - CSS (Incompleto)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,11 @@ import { Router } from 'express';
 import * as LoginController from '../controllers/loginController';
 import * as passwordRecoverController from '../controllers/passwordRecoverController';
 import * as SignInController from '../controllers/signInController';
+import * as IndexController from '../controllers/indexController';
 
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.send('home');
-});
-
+router.get('/', IndexController.index);
 router.get('/login', LoginController.login);
 router.get('/passwordRecover', passwordRecoverController.passwordRecover);
 router.get('/signIn', SignInController.signIn);
